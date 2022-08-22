81c4be6249501f736fc5699c8222e01f50323a38
Andrey <folganoid@yahoo.com>
2022-08-22 11:40:02 +0300

    copyplugin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+import * as $ from 'jquery'
 import Post from './Post'
 import json from './assets/json.json'
-import xml from './assets/data.xml'
-import csv from './assets/data.csv'
+// import xml from './assets/data.xml'
+// import csv from './assets/data.csv'
 import Logo from './assets/bird.png'
 import './styles/styles.css'
 
 const post = new Post('Webpack Post Title');
+
+$('pre').html(post.toString())
+
 console.log("Post to string: ", post.toString());
 console.log('JSON: ', json)
-console.log('XML: ', xml)
-console.log('CSV: ', csv)
+// console.log('XML: ', xml)
+// console.log('CSV: ', csv)
